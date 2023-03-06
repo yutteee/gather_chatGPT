665f96164ac268b1cb48d7ae47f4077b10d8343b
yutteee <yusaku_nakamura_aa@icloud.com>
2023-03-07 01:11:22 +0900

    gatherに接続できた #1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+require("dotenv").config();
+const { Game } = require("@gathertown/gather-game-client");
+global.WebSocket = require("isomorphic-ws");
+
+/**** setup ****/
+
+// gatherに接続する
+// 参考:https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063
+const game = new Game(process.env.SPACE_ID, () => Promise.resolve({ apiKey: process.env.API_KEY }));
+game.connect();
+game.subscribeToConnection((connected) => console.log("connected?", connected));
