2ea93688f3b4d7f9bd0afea69ee9ff05e67f60a7
yutteee <yusaku_nakamura_aa@icloud.com>
2023-03-07 19:42:14 +0900

    roomに対して返事をしないようにした #3

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const replyFromChatGPT = async function (message) {
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: message }],
   });
+  console.log(completion.data.choices[0].message.content);
   return completion.data.choices[0].message.content;
 };
 
@@ -24,9 +25,13 @@ const game = new Game(process.env.SPACE_ID, () => Promise.resolve({ apiKey: proc
 game.connect();
 game.subscribeToConnection((connected) => console.log("connected?", connected));
 
+BOT_NAME = "gather_chatGPT";
+
 game.subscribeToEvent("playerChats", (data, context) => {
-  // chatGPTの応答にイベントが走らないようにする
-  if (data.playerChats.senderId === process.env.BOT_ID) return;
+  // chatGPTの応答に対して、イベントが走らないようにする
+  if (data.playerChats.senderName === BOT_NAME) return;
+  // roomでのメッセージに対して返答しないようにする
+  if (data.playerChats.recipient === "GLOBAL_CHAT") return;
 
   const receivedMessage = data.playerChats.contents;
   const chatRecipient = data.playerChats.recipient; // 個人間のメッセージ, nearby, Everyoneのいずれか
