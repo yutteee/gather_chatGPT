6fdfbf687a47c0e2286a17a9886c02b923354056
yutteee <yusaku_nakamura_aa@icloud.com>
2023-03-08 22:53:51 +0900

    ファイル実行時にbotが生成するようにした #5

--- index.js
+++ index.js
@@ -35,11 +35,14 @@ const replyFromChatGPT = async function (message) {
 // 参考:https://gathertown.notion.site/Gather-Websocket-API-bf2d5d4526db412590c3579c36141063
 const game = new Game(process.env.SPACE_ID, () => Promise.resolve({ apiKey: process.env.API_KEY }));
 game.connect();
-game.subscribeToConnection((connected) => {
-  console.log("connected?", connected);
-});
+const onConnected = (connected) => {
+  console.log("Connected: ", connected);
+  // ファイル実行時にアバターを生成
+  // 参考：http://gather-game-client-docs.s3-website-us-west-2.amazonaws.com/classes/Game.html#enter
+  game.enter(process.env.SPACE_ID);
+};
+game.subscribeToConnection(onConnected);
 
-// chatGPTくんの座標を取得する関数
 const searchBotCoordinate = function () {
   const botPlayers = Object.values(game.players).filter((player) => player.name === BOT_NAME);
   return botPlayers.map((player) => ({ x: player.x, y: player.y }));
@@ -65,9 +68,9 @@ const nearbyPlayersIds = function () {
 };
 
 game.subscribeToEvent("playerChats", (data, context) => {
-  // DM以外では、%を先頭につけていないと受け取らないようにした
+  // DM以外では、%を先頭につけていないと受け取らない
   if (data.playerChats.messageType !== "DM" && data.playerChats.contents.charAt(0) !== "%") return;
-  if (data.playerChats.senderName === BOT_NAME) return; // chatGPTの応答に対して、イベントが走らないようにする
+  if (data.playerChats.senderName === BOT_NAME) return; // chatGPTの応答に対して、イベントが走るのを防ぐ
   if (isReplying) return console.log("chatgptが返信を考えてるよ!");
 
   const receivedMessage = data.playerChats.contents.substring(1);
